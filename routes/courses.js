f3e2ee17e1a5684b73e491dbe1e5335e35145dd3
SimonNC <simon.jorite@gmail.com>
2024-01-19 09:50:04 +1100

    GET single course, add course, update course and delete course methods

--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const { getCourses } = require("../controllers/courses");
+const {
+  getCourses,
+  getCourse,
+  createCourse,
+  updateCourse,
+  deleteCourse,
+} = require("../controllers/courses");
 
 const router = express.Router({ mergeParams: true });
 
-router.route("/").get(getCourses);
+router.route("/:id").get(getCourse).put(updateCourse).delete(deleteCourse);
+router.route("/").get(getCourses).post(createCourse);
 
 module.exports = router;
